0f8a408fe0946d988698317d7474a53a82091cc2
Johann Schneider <joschi.dev7@gmail.com>
2025-08-27 11:52:45 +0200

    block_to_block_type has been implemented and tested.

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,28 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "Paragraph"
+    HEADING = "Heading"
+    CODE = "Code"
+    QUOTE = "Quote"
+    UNORDERED_LIST = "UnorderedList"
+    ORDERED_LIST = "OrderedList"
+
+def block_to_block_type(block):
+    if block.startswith("#") or block.startswith("##") or block.startswith("###") or block.startswith("####") or block.startswith("#####") or block.startswith("######"):
+        return BlockType.HEADING
+    elif block.startswith(">>>") and block.endswith("<<<"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        return BlockType.UNORDERED_LIST
+    elif block[0].isdigit() and block[1] == "." and block[2] == " ":
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+
 def markdown_to_blocks(markdown):
 
     blocks = markdown.split("\n\n")
@@ -7,6 +32,8 @@ def markdown_to_blocks(markdown):
         if not blocks[i]:
             blocks.remove(blocks[i])
     return blocks
+
+
 
 if __name__ == "__main__":
     md = """This is **bolded** paragraph """
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,5 +1,6 @@
 import unittest
 from markdown import markdown_to_blocks
+from markdown import block_to_block_type, BlockType
 
 class TestMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -19,3 +20,26 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+        
+    def test_block_to_block_type(self):
+        
+
+        md = """ This is **bolded** paragraph 
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items"""
+        blocks = markdown_to_blocks(md)
+
+        for block in blocks:
+            if block == "This is **bolded** paragraph":
+                self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+            elif block == "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line":
+                self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+            elif block == "- This is a list\n- with items":
+                self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+            else:
+                self.fail(f"Unexpected block: {block}")
