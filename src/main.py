fa66c77af7c1fa72a8a6d028d94171fe8dbf8a98
Johann Schneider <joschi.dev7@gmail.com>
2025-08-30 11:46:09 +0200

    another try with copy function of boot.dev

--- src/main.py
+++ src/main.py
@@ -10,28 +10,21 @@ def main():
     basepath = "/"
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
-    copy_content()
+    copy_files_recursive("./static", "./docs")
     generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
-def copy_content(from_p = "./static", destination = "./docs", first = True, files = []):
-    destination_path = os.path.join(destination)
-    from_path = os.path.join(from_p)
-    if first and os.path.exists(destination_path):
-        print(f"removing {destination_path} if it exists")
-        shutil.rmtree(destination_path, ignore_errors=True)
-        print(f"recreating {destination_path}")
-        os.mkdir(destination_path)
-    if os.path.exists(from_path):
-        list_of_files = os.listdir(from_path)
-        for list_file in list_of_files:
-            if os.path.isfile(os.path.join(from_path, list_file)):
-                print(f"copying {list_file} to {destination_path}")
-                shutil.copy(os.path.join(from_path, list_file), destination_path)
-            else:
-                dir = os.path.join(destination_path, list_file)
-                print(f"creating {dir}")
-                os.mkdir(dir)
-                copy_content(from_p=from_p + "/" + list_file, destination=dir, first = False, files=files)
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
             
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
